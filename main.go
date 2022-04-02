b430a2c15e71b28d1b5ebb61d373990b47038f4d
Dao Pham <daopm.dean@gmail.com>
2022-04-02 23:08:39 +0700

    unmarshal

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 type person struct {
@@ -10,12 +11,23 @@ type person struct {
 }
 
 func main() {
-	p1 := person{First: "Dean"}
-	p2 := person{First: "Soul"}
+	p1 := person{
+		First: "Dean",
+	}
+	p2 := person{
+		First: "Soul",
+	}
 	xp := []person{p1, p2}
 	byts, err := json.Marshal(xp)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-	fmt.Println(string(byts))
+	fmt.Println("MARSHAL", string(byts))
+
+	xp2 := []person{}
+	err = json.Unmarshal(byts, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("UNMARSHAL", xp2)
 }
