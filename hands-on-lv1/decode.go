f9451f49cee36484a2b08973b90cadf55eae9937
Dao Pham <daopm.dean@gmail.com>
2022-04-03 22:35:45 +0700

    hands on lv1 ex2

---
+++ hands-on-lv1/decode.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8082", nil)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	slices := []person{}
+	json.NewDecoder(r.Body).Decode(&slices)
+	log.Println(slices)
+}
--- hands-on-lv1/encode.go
+++ hands-on-lv1/encode.go
@@ -1,4 +1,4 @@
-package main
+package handsonlv1
 
 import (
 	"encoding/json"
