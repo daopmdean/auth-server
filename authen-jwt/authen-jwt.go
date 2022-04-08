8f3446604f76fdb3a660d26d5275c55c8ff76547
Dao Pham <daopm.dean@gmail.com>
2022-04-08 11:24:11 +0700

    getJwt

--- authen-jwt/authen-jwt.go
+++ authen-jwt/authen-jwt.go
@@ -1,1 +1,102 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/submit", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+type myClaims struct {
+	jwt.StandardClaims
+	Email string
+}
+
+func getJwt(msg string) (string, error) {
+	key := []byte("My Secret Key")
+
+	claims := myClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+			Issuer:    "authen-jwt",
+		},
+		Email: msg,
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+
+	s, err := t.SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("Error in getJwt %w", err)
+	}
+
+	return s, nil
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	code, err := getJwt(email)
+	if err != nil {
+		http.Error(w, "Could not get Jwt", http.StatusInternalServerError)
+		return
+	}
+
+	c := http.Cookie{
+		Name:  "session",
+		Value: code + "|" + email,
+	}
+
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{}
+	}
+
+	isLoggedIn := false
+
+	message := "Not Logged In"
+	if isLoggedIn {
+		message = "Logged In"
+	}
+
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>HMAC Example</title>
+	</head>
+	<body>
+		<p>` + c.Value + `</p>
+		<p>` + message + `</p>
+		<form action="/submit" method="post">
+			<input type="email" name="email" />
+			<input type="submit" />
+		</form>
+	</body>
+	</html>`
+	io.WriteString(w, html)
+}
