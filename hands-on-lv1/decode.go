358eba22a92034b4662137451a70493ffa222fed
Dao Pham <daopm.dean@gmail.com>
2022-04-08 10:47:07 +0700

    refactor hands on lv 1

--- hands-on-lv1/decode.go
+++ hands-on-lv1/decode.go
@@ -1,4 +1,4 @@
-package main
+package handsonlv1
 
 import (
 	"encoding/json"
@@ -6,14 +6,14 @@ import (
 	"net/http"
 )
 
-type person struct {
-	First string
-}
+// type person struct {
+// 	First string
+// }
 
-func main() {
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8082", nil)
-}
+// func main() {
+// 	http.HandleFunc("/decode", bar)
+// 	http.ListenAndServe(":8082", nil)
+// }
 
 func bar(w http.ResponseWriter, r *http.Request) {
 	slices := []person{}
