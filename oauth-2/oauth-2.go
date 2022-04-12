5eb40f0581c37952742bc0cfb4235e1be81333ce
Dao Pham <daopm.dean@gmail.com>
2022-04-12 14:52:56 +0700

    oauth2 make api request

--- oauth-2/oauth-2.go
+++ oauth-2/oauth-2.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
+
+const githubGraphqlApi = "https://api.github.com/graphql"
+const jsonContentType = "application/json"
 
 var githubOauthConfig = &oauth2.Config{
 	ClientID:     "8ecab7653a8b3804c2c8",
@@ -16,7 +22,8 @@ var githubOauthConfig = &oauth2.Config{
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/oauth/github", githubOauth)
+	http.HandleFunc("/oauth2/github", githubOauth)
+	http.HandleFunc("/oauth2/receive", githubOauthHandleReceive)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -28,7 +35,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		<title>OAuth2 Example</title>
 	</head>
 	<body>
-		<form action="/oauth/github" method="post">
+		<form action="/oauth2/github" method="post">
 			<input type="email" name="email" />
 			<input type="submit" value="Github Login"/>
 		</form>
@@ -38,11 +45,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func githubOauth(w http.ResponseWriter, r *http.Request) {
-	redirectUrl := githubOauthConfig.AuthCodeURL("state8888")
+	redirectUrl := githubOauthConfig.AuthCodeURL("8888")
 	http.Redirect(w, r, redirectUrl, http.StatusSeeOther)
 }
 
 func githubOauthHandleReceive(w http.ResponseWriter, r *http.Request) {
+	log.Println("processing receive...")
+
 	state := r.FormValue("state")
 	if state != "8888" {
 		http.Error(w, "Invalid State", http.StatusBadRequest)
@@ -59,4 +68,20 @@ func githubOauthHandleReceive(w http.ResponseWriter, r *http.Request) {
 
 	src := githubOauthConfig.TokenSource(ctx, token)
 	client := oauth2.NewClient(ctx, src)
+	requestBody := strings.NewReader(`{
+		"query": "query {viewer {id}}"
+	}`)
+	res, err := client.Post(githubGraphqlApi, jsonContentType, requestBody)
+	if err != nil {
+		http.Error(w, "Could not make request to github api", http.StatusInternalServerError)
+		return
+	}
+
+	bs, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, "Could not read response body", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println(string(bs))
 }
