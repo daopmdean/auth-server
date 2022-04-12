8f07a65ded5f428b6bc6271883f2d008c3877953
Dao Pham <daopm.dean@gmail.com>
2022-04-12 10:58:30 +0700

    oauth2 index

--- oauth-2/oauth-2.go
+++ oauth-2/oauth-2.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("oauth")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>OAuth2 Example</title>
+	</head>
+	<body>
+		<form action="/oauth" method="post">
+			<input type="email" name="email" />
+			<input type="submit" value="Github Login"/>
+		</form>
+	</body>
+	</html>`
+	fmt.Fprint(w, html)
 }
