a70b6b6863c34884cfbcb75889f1bbc94a6925e1
Dao Pham <daopm.dean@gmail.com>
2022-04-12 22:30:28 +0700

    oauth2 google

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.65.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	google.golang.org/appengine v1.6.6 // indirect
--- hands-on-lv3/excercise-3.go
+++ hands-on-lv3/excercise-3.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -10,6 +12,7 @@ import (
 	"github.com/google/uuid"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
+	"golang.org/x/oauth2/google"
 )
 
 const githubGraphqlApi = "https://api.github.com/graphql"
@@ -19,6 +22,17 @@ var githubOauthConfig = &oauth2.Config{
 	ClientID:     "8ecab7653a8b3804c2c8",
 	ClientSecret: "12722aeebc07090cf0e084f9fdb0fb0ac0cac44a",
 	Endpoint:     github.Endpoint,
+}
+
+var googleOauthConfig = &oauth2.Config{
+	ClientID:     "167611725265-is1oj5liqvtr5afpn6t9cfddb4bi5a8r.apps.googleusercontent.com",
+	ClientSecret: "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",
+	RedirectURL:  "http://localhost:8080/oauth2/google/receive",
+	Scopes: []string{
+		"https://www.googleapis.com/auth/userinfo.profile",
+		"https://www.googleapis.com/auth/userinfo.email",
+	},
+	Endpoint: google.Endpoint,
 }
 
 type githubRes struct {
@@ -36,6 +50,8 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/oauth2/github", githubOauth)
 	http.HandleFunc("/oauth2/receive", githubOauthHandleReceive)
+	http.HandleFunc("/oauth2/google", googleOauth)
+	http.HandleFunc("/oauth2/google/receive", googleOauthHandleReceive)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -50,6 +66,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 		<form action="/oauth2/github" method="post">
 			<input type="submit" value="Github Login"/>
 		</form>
+		</br>
+		<form action="/oauth2/google" method="post">
+			<input type="submit" value="Google Login"/>
+		</form>
 	</body>
 	</html>`
 	fmt.Fprint(w, html)
@@ -61,7 +81,7 @@ func githubOauth(w http.ResponseWriter, r *http.Request) {
 }
 
 func githubOauthHandleReceive(w http.ResponseWriter, r *http.Request) {
-	log.Println("processing receive...")
+	log.Println("Processing github oauth receive...")
 
 	state := r.FormValue("state")
 	if state != "8888" {
@@ -102,4 +122,52 @@ func githubOauthHandleReceive(w http.ResponseWriter, r *http.Request) {
 
 	log.Println(appUserId)
 	log.Println(githubConnections)
+}
+
+func googleOauth(w http.ResponseWriter, r *http.Request) {
+	redirectUrl := googleOauthConfig.AuthCodeURL("8888")
+	http.Redirect(w, r, redirectUrl, http.StatusSeeOther)
+}
+
+func googleOauthHandleReceive(w http.ResponseWriter, r *http.Request) {
+	log.Println("Processing google oauth receive...")
+
+	state := r.FormValue("state")
+	if state != "8888" {
+		http.Error(w, "Invalid State", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	code := r.FormValue("code")
+	token, err := googleOauthConfig.Exchange(ctx, code)
+	if err != nil {
+		http.Error(w, "Could not login", http.StatusInternalServerError)
+		return
+	}
+
+	src := googleOauthConfig.TokenSource(ctx, token)
+	client := oauth2.NewClient(ctx, src)
+
+	res, err := client.Get("https://www.googleapis.com/oauth2/v1/userinfo?alt=json")
+	if err != nil {
+		http.Error(w, "Could not get google info", http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	err = printResponseBody(res.Body)
+	if err != nil {
+		http.Error(w, "Could not read info", http.StatusInternalServerError)
+		return
+	}
+}
+
+func printResponseBody(body io.Reader) error {
+	bs, err := ioutil.ReadAll(body)
+	if err != nil {
+		return err
+	}
+	log.Println(string(bs))
+	return nil
 }
