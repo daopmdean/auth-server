cbc854f898ad1721260076a46a28d97e53a04dc5
Dao Pham <daopm.dean@gmail.com>
2022-04-12 11:26:23 +0700

    oauth2 github

--- go.mod
+++ go.mod
@@ -6,4 +6,12 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- oauth-2/oauth-2.go
+++ oauth-2/oauth-2.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
 )
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "8ecab7653a8b3804c2c8",
+	ClientSecret: "12722aeebc07090cf0e084f9fdb0fb0ac0cac44a",
+	Endpoint:     github.Endpoint,
+}
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/oauth/github", githubOauth)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -18,11 +28,16 @@ func index(w http.ResponseWriter, r *http.Request) {
 		<title>OAuth2 Example</title>
 	</head>
 	<body>
-		<form action="/oauth" method="post">
+		<form action="/oauth/github" method="post">
 			<input type="email" name="email" />
 			<input type="submit" value="Github Login"/>
 		</form>
 	</body>
 	</html>`
 	fmt.Fprint(w, html)
+}
+
+func githubOauth(w http.ResponseWriter, r *http.Request) {
+	redirectUrl := githubOauthConfig.AuthCodeURL("state8888")
+	http.Redirect(w, r, redirectUrl, http.StatusSeeOther)
 }
